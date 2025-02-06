398209468a14a3fe6cb16239839455406d4666c6
udvardiendre <udvardiendre@gmail.com>
2025-02-06 23:16:26 +0100

    finished till contact info

--- components/landing/contact-info.tsx
+++ components/landing/contact-info.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Envelope, PhoneWhite } from "../icons";
 
-type Props = {}
+type Props = {};
 
 const ContactInfo = (props: Props) => {
   return (
-    <div>ContactInfo</div>
-  )
-}
+    <section className="w-full px-5 md:px-10 md:py-20 py-10">
+      <div className="container mx-auto">
+        <div className="bg-white shadow-xl rounded-[30px] max-w-[931px] flex flex-col lg:flex-row items-center p-[1.5rem] md:p-[2.25rem] gap-6 lg:justify-between mx-auto">
+          <div className="flex items-center gap-[14px]">
+            <div className="bg-primary-blue p-2.5 md:p-5 rounded-full w-[43px] h-[43px]  md:w-[83px] md:h-[83px] flex items-center justify-center">
+              <Envelope />
+            </div>
+            <p className="text-[16px]/[26px] md:text-[25px]/[37px] font-semibold">
+              scoutlyapp@gmail.com
+            </p>
+          </div>
+          <div className="lg:h-[100px] w-full h-1 lg:w-px bg-[#F0F0F0]" />
+          <div className="flex items-center gap-[14px]">
+            <div className="bg-primary-blue p-2.5 md:p-5 rounded-full w-[43px] h-[43px]  md:w-[83px] md:h-[83px] flex items-center justify-center">
+              <PhoneWhite />
+            </div>
+            <p className="text-[16px]/[26px] md:text-[25px]/[37px] font-semibold">
+              +46 (70) 320 0713
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ContactInfo
+export default ContactInfo;
--- components/landing/our-motivation.tsx
+++ components/landing/our-motivation.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import ScoutlyLogo from "@/assets/landing/scoutly_logo_motiv.png";
 
-type Props = {}
-
-const OurMotivation = (props: Props) => {
+const OurMotivation = () => {
   return (
-    <div>OurMotivation</div>
-  )
-}
+    <section className="w-full px-5 md:px-10 md:py-40 py-20">
+      <div className="container mx-auto flex items-center justify-center bg-motivation-bg bg-cover min-h-[398px] rounded-[40px] flex-col gap-3 md:gap-6 py-8">
+        <div className="flex flex-col md:flex-row items-center gap-2">
+          <h2 className="text-primary-blue text-[19px]/[24px] md:text-[2.375rem]/[2.9562rem] font-bold uppercase">
+            Our motivation at
+          </h2>
+          <Image
+            className="w-[190px] h-auto"
+            src={ScoutlyLogo}
+            alt="scoutly-logo-2"
+          />
+        </div>
+        <p className="text-center text-description text-primary-blue max-w-[80%] md:max-w-[45%]">
+          At Scoutly, we&apos;re driven by a passion to connect hockey players and
+          teams. Our goal is to simplify recruitment, foster talent discovery,
+          and build a thriving community that empowers hockey&apos;s future.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default OurMotivation
+export default OurMotivation;
